ed4fe2ad5e010835759531af4a9f535a8f589862
RicoJia <ruotongjia2020@u.northwestern.edu>
2020-01-12 09:43:48 -0600

    finished xacro and launch file!

--- rigid2d/src/test_compile.sh
+++ rigid2d/src/test_compile.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 g++ -o ./test.o ./test1.cpp ./rigid2d.cpp
-#./test.o
 
 if [ -a ./test1_input.txt ];
 then
   echo "test1_input.txt is found!"
-#  ./test.o<./test1_input.txt 2>&1
   if ./test.o<./test1_input.txt | cmp ./test1_answer.txt;
   then
     echo "Success!"
